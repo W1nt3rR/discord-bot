90e0620062c9c0be31cb5894fed89a0819515dc2
W1nt3rR <frostywinterr@gmail.com>
2022-04-26 12:48:01 +0200

    change from int to float

--- commands/mul.js
+++ commands/mul.js
@@ -11,8 +11,8 @@ module.exports = {
 		// const now = Date.now();
 		// const duration = (now - msg.createdTimestamp) / 1000;
 		// msg.reply(`Pong! Time needed: ${duration.toFixed(3)}s`);
-        var a = parseInt(args[0]);
-        var b = parseInt(args[1]);
+        var a = parseFloat(args[0]);
+        var b = parseFloat(args[1]);
         msg.reply(`Multiplication of two numbers is ${a * b}`);
 	},
 };
